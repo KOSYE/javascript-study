5415459eb1a60f486fab5d680784445486a112be
KOSYE <ijn030203@gmail.com>
2023-10-31 17:20:24 +0900

    배워가는중

--- java_script/var.js
+++ java_script/var.js
@@ -1,12 +1,38 @@
-var a = 2 // 변수
-let b = 'fuck'
-const c = 4
-console.log("hello world!")
-// console.log(a)
-// console.log(b)
-// console.log(c)
 
-console.log(a*c)
-console.log(3+1)
-console.log("3"+"1")
-console.log("3"+1)
+
+
+// console.log("hello world!")
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+
+// console.log(3+1)
+// console.log("3"+"1")
+// console.log("3"+1)
+
+// let name = "KIMHWAN"
+// age = 21;
+
+// console.log(name)
+// const message1 = "I'm-a-boy." // 중요
+// const message2 = 'I\n'+'m-a-boy.' // 거의 안쓰임
+// const message3 = `My-name-is-${name}` // 중요
+// const message4 = `나는 ${age+1}살 입니다.`
+// console.log(message2)
+
+// // a = 3
+// console.log(3)
+// console.log(message3)
+// console.log(message4)
+
+// // +     -    * 곱하기   / 나누기   % 나머지
+// console.log(8%3)
+
+
+
+
+//Bollean
+const a = true //참
+const b = false //거짓
+console.log(b)
